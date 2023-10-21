6113d5fea943d6b4674a36c2a20888031a4145a9
SUJALSINGH7473 <singhsujal466@gmail.com>
2023-10-21 15:50:30 +0530

    create pagese and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Home from './pages/Home';
 export default function App() {
-  return (
-    <div className='text-red-400'>
-      Hello, ki haal tuada
-    </div>
-  )
+  return(<BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-out' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+    </Routes>
+  </BrowserRouter>)
 }
