8b290afcf18cb67828ca3ebb38693dfaebbed1f6
SUJALSINGH7473 <singhsujal466@gmail.com>
2023-10-25 19:33:16 +0530

    Add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Signin from './pages/Signin';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import About from './pages/About';
-import Home from './pages/Home';
-import Header from './components/Header';
+
+import SignUp from './pages/SignUp.jsx';
+import Profile from './pages/Profile.jsx';
+import About from './pages/About.jsx';
+import Home from './pages/Home.jsx';
+import Header from './components/Header.jsx';
+import SignIn from './pages/SignIn.jsx';
 export default function App() {
   return(<BrowserRouter>
     <Header/>
     <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
         <Route path='/profile' element={<Profile/>}/>
         <Route path='/about' element={<About/>}/>
