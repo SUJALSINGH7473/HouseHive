4bb96c1d7522b17a1e255d059e7262cbdd867e9d
SUJALSINGH7473 <singhsujal466@gmail.com>
2023-11-04 15:09:27 +0530

    create user update api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose, { mongo } from "mongoose";
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
+import authRouter from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -16,12 +17,14 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
 app.use((err, req, res, next)=>{
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -43,27 +43,60 @@ export default function Profile() {
       });
   };
   return (
-    <div className="max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
-        <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"></input>
-        <img
-          onClick={()=>fileRef.current.click()} className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-          src={formData.avatar || currentUser.avatar}
-          alt="profile"
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+      <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
         />
-        <p className="text-sm text-center">
-        {fileUploadError? (<span className="text-red-700">Error Image Upload(image must be less than 2mb)</span>):
-        filePerc>0 && filePerc<100 ? (<span className="text-slate-700">{`Uploading ${filePerc}%`}</span>):(filePerc==100?(<span className="text-green-700">Image Successfully Uploaded!</span>):"" )}
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
         </p>
-        <input type="text" id="username" placeholder="username" className="border p-3 rounded-lg" ></input>
-        <input type="email" id="email" placeholder="email" className="border p-3 rounded-lg" ></input>
-        <input type="text" id="password" placeholder="password" className="border p-3 rounded-lg" ></input>
-        <button className="bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80">Update</button>      
-      </form>
-      <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <input
+          type='text'
+          placeholder='username'
+          id='username'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='text'
+          placeholder='password'
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
+          update
+        </button>
+        </form>
+        <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
     </div>
   );
